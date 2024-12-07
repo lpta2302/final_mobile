5ae2e2be19ba27f115fb789da964df470cd11d77
lpta2302 <lpta2302@gmail.com>
2024-12-07 22:03:17 +0700

    finish layout

--- app/src/main/java/com/dev/mail/lpta2302/final_mobile/MainActivity.java
+++ app/src/main/java/com/dev/mail/lpta2302/final_mobile/MainActivity.java
@@ -1,45 +1,93 @@
 package com.dev.mail.lpta2302.final_mobile;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import com.dev.mail.lpta2302.final_mobile.post.Post;
-import com.dev.mail.lpta2302.final_mobile.post.PostService;
-import com.dev.mail.lpta2302.final_mobile.util.QueryCallback;
+import com.dev.mail.lpta2302.final_mobile.databinding.ActivityMainBinding;
+import com.dev.mail.lpta2302.final_mobile.fragments.CreateFragment;
+import com.dev.mail.lpta2302.final_mobile.fragments.FriendsFragment;
+import com.dev.mail.lpta2302.final_mobile.fragments.HomeFragment;
+import com.dev.mail.lpta2302.final_mobile.fragments.LikedFragment;
+import com.dev.mail.lpta2302.final_mobile.fragments.MenuFragment;
+import com.dev.mail.lpta2302.final_mobile.fragments.NotificationFragment;
+import com.dev.mail.lpta2302.final_mobile.fragments.SearchFragment;
+import com.dev.mail.lpta2302.final_mobile.global.AuthUser;
+import com.google.android.material.navigation.NavigationBarView;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    Toolbar toolbar;
+    private  final Map<Integer, Fragment> fragmentMap = new HashMap<Integer, Fragment>(){};
+    private final NavigationBarView.OnItemSelectedListener selectListener = new NavigationBarView.OnItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            int id = item.getItemId();
+            if(id == R.id.home)
+                replaceFragment(new HomeFragment());
+            if(id == R.id.liked)
+                replaceFragment(new LikedFragment());
+            if(id == R.id.friends)
+                replaceFragment(new FriendsFragment());
+            if(id == R.id.create)
+                replaceFragment(new CreateFragment());
+            if(id == R.id.menu)
+                replaceFragment(new MenuFragment());
+            return true;
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.notification){
+            replaceFragment(new NotificationFragment());
+        } else if (id == R.id.search){
+            replaceFragment(new SearchFragment());
+        }
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        if (AuthUser.getInstance().isAuthenticated()){
+            binding = ActivityMainBinding.inflate(getLayoutInflater());
+            setContentView(binding.getRoot());
+            toolbar = findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
 
-        PostService.getInstance().searchPostsByCaption("ted", new QueryCallback<List<Post>>() {
-            @Override
-            public void onSuccess(List<Post> posts) {
-                posts.forEach(post->{
-                    Log.d("post", post.toString());
-                });
-            }
-
-            @Override
-            public void onFailure(Exception e) {
-
-            }
-        });
+            replaceFragment(new HomeFragment());
+            binding.bottomNavigationView.setBackground(null);
+            binding.bottomNavigationView.setOnItemSelectedListener(selectListener);
+        } else{
+//            Intent intent = new Intent(this, LoginActivity.class); //add login acctivity to change
+//            startActivity(intent);
+            finish();
+        }
     }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+
 }
