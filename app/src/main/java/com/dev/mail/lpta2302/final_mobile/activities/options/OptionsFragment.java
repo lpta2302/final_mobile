1126567b938b354d6e35e8d9f237f75cb2904e15
lpta2302 <lpta2302@gmail.com>
2024-12-08 19:08:50 +0700

    add placeholder

--- app/src/main/java/com/dev/mail/lpta2302/final_mobile/activities/options/OptionsFragment.java
+++ app/src/main/java/com/dev/mail/lpta2302/final_mobile/activities/options/OptionsFragment.java
@@ -2,27 +2,71 @@ package com.dev.mail.lpta2302.final_mobile.activities.options;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.dev.mail.lpta2302.final_mobile.ChangePassword;
 import com.dev.mail.lpta2302.final_mobile.ManageProfile;
 import com.dev.mail.lpta2302.final_mobile.R;
+import com.dev.mail.lpta2302.final_mobile.activities.home.PostAdapter;
+import com.dev.mail.lpta2302.final_mobile.post.Post;
+import com.dev.mail.lpta2302.final_mobile.post.PostService;
+import com.dev.mail.lpta2302.final_mobile.util.QueryCallback;
 import com.google.android.material.chip.Chip;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class OptionsFragment extends Fragment {
 
-    @Nullable
+    private RecyclerView recyclerView;
+    private PostAdapter postAdapter;
+    private List<Post> postList;
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // Inflate layout for this fragment
-        View root = inflater.inflate(R.layout.fragment_options, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_options, container, false);
 
-        return root;
+        // Initialize RecyclerView and data
+        recyclerView = rootView.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        postList = new ArrayList<>();
+        postAdapter = new PostAdapter(postList);
+        recyclerView.setAdapter(postAdapter);
+
+        return rootView;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        PostService.getInstance().readPosts(new QueryCallback<List<Post>>() {
+            @Override
+            public void onSuccess(List<Post> expectation) {
+                Log.d("found",String.valueOf(expectation.size()));
+                postList.clear();
+                postList.addAll(expectation);
+                postAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Exception exception) {
+                postList = new ArrayList<>();
+                postAdapter = new PostAdapter(postList);
+                recyclerView.setAdapter(postAdapter);
+            }
+        });
+    }
+
 }
