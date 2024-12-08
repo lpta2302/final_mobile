6c969f74a62066cc98467c675db342be399a7ca8
lpta2302 <lpta2302@gmail.com>
2024-12-08 18:31:59 +0700

    fix

--- app/src/main/java/com/dev/mail/lpta2302/final_mobile/activities/options/OptionsFragment.java
+++ app/src/main/java/com/dev/mail/lpta2302/final_mobile/activities/options/OptionsFragment.java
@@ -23,50 +23,6 @@ public class OptionsFragment extends Fragment {
         // Inflate layout for this fragment
         View root = inflater.inflate(R.layout.fragment_options, container, false);
 
-        // Nhận các ChipButton
-        Chip chipEditProfile = root.findViewById(R.id.chipEditProfile);
-        Chip chipChangePassword = root.findViewById(R.id.chipChangePassword);
-        Chip chipLogout = root.findViewById(R.id.chipLogout);
-
-        // Sự kiện nhấn chip "Chỉnh sửa hồ sơ"
-        chipEditProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(requireActivity(), ManageProfile.class);
-                startActivity(intent);
-            }
-        });
-
-        // Sự kiện nhấn chip "Đổi mật khẩu"
-        chipChangePassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(requireActivity(), ChangePassword.class);
-                startActivity(intent);
-            }
-        });
-
-//        // Sự kiện nhấn chip "Đăng xuất"
-//        chipLogout.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                // Hiển thị AlertDialog
-//                new AlertDialog.Builder(requireContext())
-//                        .setMessage("Bạn có chắc chắn muốn thoát?")
-//                        .setCancelable(false)
-//                        .setPositiveButton("Có", new DialogInterface.OnClickListener() {
-//                            public void onClick(DialogInterface dialog, int id) {
-//                                // Chuyển hướng về trang Login
-//                                Intent intent = new Intent(requireActivity(), MainActivity.class);
-//                                startActivity(intent);
-//                                requireActivity().finish(); // Đóng activity hiện tại
-//                            }
-//                        })
-//                        .setNegativeButton("Không", null) // Đóng dialog nếu chọn "Không"
-//                        .show();
-//            }
-//        });
-
         return root;
     }
 }
